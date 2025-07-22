d5852da97595bbc9deb43b744d1aa523dfb734f0
Markus Born <markus@brunokrams.de>
2025-07-22 14:27:33 +0200

    Added tests.

---
+++ src/main/java/de/brunokrams/lichessclient/model/chess/PawnMove.java
@@ -0,0 +1,84 @@
+package de.brunokrams.lichessclient.model.chess;
+
+import java.util.List;
+
+import static de.brunokrams.lichessclient.model.chess.File.*;
+
+public class PawnMove implements Move {
+
+    private final File file;
+    private final MoveType moveType;
+    private final Field targetField;
+    private final Piece promotionPiece;
+    private final boolean enPassant;
+
+    PawnMove(File file, MoveType moveType, Field targetField, Piece promotionPiece, boolean enPassant) {
+        this.file = file;
+        this.moveType = moveType;
+        this.targetField = targetField;
+        this.promotionPiece = promotionPiece;
+        this.enPassant = enPassant;
+        if (moveType == MoveType.MOVE) {
+            if (file != null) {
+                throw new IllegalArgumentException("File is set for non capturing move.");
+            }
+            if (enPassant) {
+                throw new IllegalArgumentException("En passant is set for non capturing move.");
+            }
+        }
+        if (moveType == MoveType.TAKE) {
+            if (file == null) {
+                throw new IllegalArgumentException("File is not set for capturing move.");
+            }
+            if (!getReachableFiles(file).contains(targetField.getFile())) {
+                throw new IllegalArgumentException("Target field is unreachable.");
+            }
+        }
+        if (targetField.getRank() == Rank.EIGHT && promotionPiece == null) {
+            throw new IllegalArgumentException("Pawn on eighth rank but no promotion piece provided.");
+        }
+        if (promotionPiece != null && targetField.getRank() != Rank.EIGHT) {
+            throw new IllegalArgumentException("Promotion piece provided though pawn is not on eighth rank.");
+        }
+    }
+
+    @Override
+    public String asSan() {
+        return (file != null ? file.asSan() : "")
+                + moveType.asSan()
+                + targetField.asSan()
+                + (promotionPiece != null ? "=" + promotionPiece.asSan() : "")
+                + (enPassant ? " e.p." : "");
+    }
+
+    public List<File> getReachableFiles(File file) {
+        switch (file) {
+            case A -> {
+                return List.of(B);
+            }
+            case B -> {
+                return List.of(A, C);
+            }
+            case C -> {
+                return List.of(B, D);
+            }
+            case D -> {
+                return List.of(C, E);
+            }
+            case E -> {
+                return List.of(D, F);
+            }
+            case F -> {
+                return List.of(E, G);
+            }
+            case G -> {
+                return List.of(F, H);
+            }
+            case H -> {
+                return List.of(G);
+            }
+            default -> throw new IllegalArgumentException("Unexpected value: " + file);
+        }
+    }
+
+}
---
+++ src/test/java/de/brunokrams/lichessclient/model/chess/PawnMoveTest.java
@@ -0,0 +1,50 @@
+package de.brunokrams.lichessclient.model.chess;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class PawnMoveTest {
+
+    @ParameterizedTest
+    @CsvSource(
+            nullValues = "null",
+            value = {
+                    "A,TAKE,C3,null,false",// Target field is not reachable from file
+                    "null,MOVE,D8,null,false",// Target field is on eighth rank but no promotion piece is provided
+                    "null,MOVE,D7,QUEEN,false",// Target field is not on eight rank but promotion piece is provided
+                    "null,MOVE,C3,null,true",// enPassant is true but movetype is move
+                    "null,MOVE,C4,null,true",// enPassant is true but targetField is no en passant field
+                    "null,TAKE,A5,null,false",// MoveType is Take but File is not provided
+                    "E,MOVE,E5,null,false",// MoveType is Move but File is provided
+            }
+    )
+    void constructor_throwsException_whenParametersAreInconsistent(File file, MoveType moveType, Field targetField, Piece promotionPiece, boolean enPassant) {
+        // when / then
+        assertThatThrownBy(() -> new PawnMove(file, moveType, targetField, promotionPiece, enPassant))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            nullValues = "null",
+            value = {"null,MOVE,E4,null,false,e4",
+                    "null,MOVE,E8,QUEEN,false,e8=Q",
+                    "E,TAKE,D3,null,true,exd3 e.p.",
+                    "E,TAKE,D4,null,false,exd4",
+                    "E,TAKE,D8,ROOK,false,exd8=R"})
+    void asSan_doesWhatItShould(File file, MoveType moveType, Field targetField, Piece promotionPiece, boolean enPassant, String expectedSan) {
+        // given
+        Move move = new PawnMove(file, moveType, targetField, promotionPiece, enPassant);
+
+        // when
+        String actualSan = move.asSan();
+
+        // then
+        assertThat(actualSan).isEqualTo(expectedSan);
+    }
+
+}
