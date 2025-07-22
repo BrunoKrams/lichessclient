92f68fd70ff9cbfbac4445ce60d00ed01beaffa5
Markus Born <markus@brunokrams.de>
2025-07-22 15:30:58 +0200

    Added more tests

--- src/main/java/de/brunokrams/lichessclient/model/chess/Piece.java
+++ src/main/java/de/brunokrams/lichessclient/model/chess/Piece.java
@@ -1,73 +1,46 @@
 package de.brunokrams.lichessclient.model.chess;
 
-import java.util.Arrays;
 import java.util.List;
 
-public interface Piece {
+public enum Piece {
 
-    String asSan();
-    List<Field> getReachableFields(Field field);
-
-    public static final Piece KNIGHT = new Piece() {
-        @Override
-        public String asSan() {
-            return "N";
-        }
-
+    KNIGHT("N") {
         @Override
         public List<Field> getReachableFields(Field field) {
-            throw new RuntimeException("Not yet implemented");
+            throw new RuntimeException("Not yet implemented.");
+        }
+    }, BISHOP("B") {
+        @Override
+        public List<Field> getReachableFields(Field field) {
+            throw new RuntimeException("Not yet implemented.");
+        }
+    }, ROOK("R") {
+        @Override
+        public List<Field> getReachableFields(Field field) {
+            throw new RuntimeException("Not yet implemented.");
+        }
+    }, QUEEN("Q") {
+        @Override
+        public List<Field> getReachableFields(Field field) {
+            throw new RuntimeException("Not yet implemented.");
+        }
+    }, KING("K") {
+        @Override
+        public List<Field> getReachableFields(Field field) {
+            throw new RuntimeException("Not yet implemented.");
         }
     };
 
-    public static final Piece BISHOP = new Piece() {
-        @Override
-        public String asSan() {
-            return "B";
-        }
+    private final String sanString;
 
-        @Override
-        public List<Field> getReachableFields(Field field) {
-            throw new RuntimeException("Not yet implemented");
-        }
-    };
+    Piece(String sanString) {
+        this.sanString = sanString;
+    }
 
-    public static final Piece ROOK = new Piece() {
-        @Override
-        public String asSan() {
-            return "R";
-        }
+    public String asSan() {
+        return sanString;
+    }
 
-        @Override
-        public List<Field> getReachableFields(Field field) {
-            throw new RuntimeException("Not yet implemented");
-        }
-    };
-
-    public static final Piece QUEEN = new Piece() {
-        @Override
-        public String asSan() {
-            return "Q";
-        }
-
-        @Override
-        public List<Field> getReachableFields(Field field) {
-            throw new RuntimeException("Not yet implemented");
-        }
-    };
-
-    public static final Piece KING = new Piece() {
-        @Override
-        public String asSan() {
-            return "K";
-        }
-
-        @Override
-        public List<Field> getReachableFields(Field field) {
-            throw new RuntimeException("Not yet implemented");
-        }
-    };
-
-
+    public abstract List<Field> getReachableFields(Field field);
 }
 
