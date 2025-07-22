aed29b9530f46b71a658d4df632c09da07242181
Markus Born <markus@brunokrams.de>
2025-07-22 14:38:59 +0200

    Added tests.

--- src/test/java/de/brunokrams/lichessclient/model/chess/PieceMoveTest.java
+++ src/test/java/de/brunokrams/lichessclient/model/chess/PieceMoveTest.java
@@ -10,6 +10,11 @@ import static org.assertj.core.api.Fail.fail;
 class PieceMoveTest {
 
     @Test
+    // BISHOP       @CsvSource({"A1,H7", "D6,E4", "E1, F3"})
+    //              @CsvSource({"A, H5", "B,G5", "G,A5"})
+    // KNIGHT       @CsvSource({"A1,E4","D6,A1", "E2, F5"})
+    //              @CsvSource({"A,E4","D,A1"})
+    // ROOK         @CsvSource({"A1,E4","D6,A1", "E2, F5"})
     void constructor_throwsException_whenFieldIsUnreachable() {
         fail();
     }
@@ -21,6 +26,11 @@ class PieceMoveTest {
 
     @Test
     void constructor_throwsException_whenUnneccessaryFieldIsProvided() {
+        fail();
+    }
+
+    @Test
+    void constructor_throwsException_whenBothFileAndFieldAreProvided() {
         fail();
     }
 
