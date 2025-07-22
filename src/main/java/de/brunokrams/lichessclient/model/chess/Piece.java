30d0dc59478a1abb3c8f9f760e57c73bc3dc3037
Markus Born <markus@brunokrams.de>
2025-07-22 16:30:14 +0200

    Implemented reachable fields.

--- src/main/java/de/brunokrams/lichessclient/model/chess/Piece.java
+++ src/main/java/de/brunokrams/lichessclient/model/chess/Piece.java
@@ -1,5 +1,7 @@
 package de.brunokrams.lichessclient.model.chess;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public enum Piece {
@@ -12,22 +14,42 @@ public enum Piece {
     }, BISHOP("B") {
         @Override
         public List<Field> getReachableFields(Field field) {
-            throw new RuntimeException("Not yet implemented.");
+            return Diagonal.getContainingDiagonals(field).stream().flatMap(diagonal -> diagonal.getFields().stream()).filter(f -> !f.equals(field)).toList();
         }
     }, ROOK("R") {
         @Override
         public List<Field> getReachableFields(Field field) {
-            throw new RuntimeException("Not yet implemented.");
+            List<Field> result = new ArrayList<>();
+            result.addAll(field.getFile().getFields());
+            result.addAll(field.getRank().getFields());
+            result.removeAll(Collections.singleton(field));
+            return result;
         }
     }, QUEEN("Q") {
         @Override
         public List<Field> getReachableFields(Field field) {
-            throw new RuntimeException("Not yet implemented.");
+            List<Field> result = new ArrayList<>();
+            result.addAll(BISHOP.getReachableFields(field));
+            result.addAll(ROOK.getReachableFields(field));
+            return result;
         }
     }, KING("K") {
         @Override
         public List<Field> getReachableFields(Field field) {
-            throw new RuntimeException("Not yet implemented.");
+            List<Field> result = new ArrayList<>();
+            char originFileChar = field.getFile().getSanChar();
+            int originRankIndex = field.getRank().getIndex();
+            for (char fileChar = (char) (originFileChar - 1); fileChar <= originFileChar + 1; fileChar++) {
+                for (int rankIndex = originRankIndex - 1; rankIndex <= originRankIndex + 1; rankIndex++) {
+                    try {
+                        result.add(Field.fromFileAndRank(File.fromChar(fileChar), Rank.fromIndex(rankIndex)));
+                    } catch (Exception e) {
+                        // Nothing to do here. Just a move which is outside of the chess board.
+                    }
+                }
+            }
+            result.removeAll(Collections.singleton(field));
+            return result;
         }
     };
 
